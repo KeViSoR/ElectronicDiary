96a090dbff25c07f05e870973667d48eb0f73dca
KeViSoR <145492005+KeViSoR@users.noreply.github.com>
2023-09-28 22:13:18 +0400

    Добавлено окно списка группы

--- ElectronicDiary/MainWindow.xaml.cs
+++ ElectronicDiary/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElectronicDiary.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,17 +21,47 @@ namespace ElectronicDiary
     /// </summary>
     public partial class MainWindow : Window
     {
-        SchoolDbContext context = new SchoolDbContext("Data Source=LAPTOP-PI8KR3G6\\SQLEXPRESS;Initial Catalog=SportSchoolVer4DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        SchoolDbContext context = new SchoolDbContext("Data Source=DESKTOP-1AUEF7A\\SQLEXPRESS;Initial Catalog=SportSchoolVer4DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
         public Department SelDepartament { get; set; }
         public Person SelTrainer { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            UpdateSportList();
         }
 
         private void SignBtn_Click(object sender, RoutedEventArgs e)
         {
 
+
+        }
+
+        private void UpdateSportList()
+        {
+            SportList.ItemsSource = context.Departments.ToList();
+        }
+
+        private void SportList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelDepartament = SportList.SelectedItem as Department;
+            CoachList.ItemsSource = context.Persons.Where(x => context.Groups.Where(z => z.DepartmentId == SelDepartament.Id).Select(y => y.Id).Contains(x.Id)).ToList();
+        }
+
+        private void CoachList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            List<CategoryGroupTrainer> listsg = new List<CategoryGroupTrainer> { };
+            List<Group> sGroup = context.Groups.ToList();
+            //InitialGroup.ItemsSource;
+            SelTrainer = CoachList.SelectedItem as Person;
+            if (SelTrainer is null) return;
+            List<Stage>? listStage = context.Groups.Where(x => x.TrainerId == SelTrainer.Id).Select(st => st.Stage).Distinct().ToList();
+            List<SportsmensGroup> listSportsmensGroup = context.SportsmensGroups.ToList();
+            if (listStage is null) return;
+            foreach (var Stage in listStage)
+            {
+                listsg.Add(new CategoryGroupTrainer(Stage, SelTrainer.Id, sGroup, listSportsmensGroup));
+            }
+            TrainerList.ItemsSource = listsg;
         }
     }
 }
